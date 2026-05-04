9bb38e8d218e6eb237cedc4160fa0738c9c94355
Allen B. Cummings <abc@maelstrom.cc>
2026-05-04 16:49:15 -0400

    #1001: Guardian implementation; added the 'per hit multiplier' concept

--- guilds/guardian-of-khazurath/defense/granite-wall.c
+++ guilds/guardian-of-khazurath/defense/granite-wall.c
@@ -1,7 +1,8 @@
 //*****************************************************************************
 // Copyright (c) 2017-2026 - Allen Cummings, RealmsMUD, All rights reserved. See
 //                      the accompanying LICENSE file for details.
-//*****************************************************************************inherit "/lib/modules/research/passiveResearchItem.c";
+//*****************************************************************************
+inherit "/lib/modules/research/passiveResearchItem.c";
 
 /////////////////////////////////////////////////////////////////////////////
 protected void Setup()
